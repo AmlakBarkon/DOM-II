1c713623a65c85b6f4433aab0718d5e5e67bca4b
Amlak befekadu <amlakbarkon@gmail.com>
2021-05-09 15:27:09 -0400

    done but still try to do css

--- js/index.js
+++ js/index.js
@@ -1,88 +1,83 @@
-//Change the body color when it is loaded;
-let bodyColor = document.querySelector("body");
-let logoHeader = document.querySelector(".logo-heading");
-
-window.addEventListener("load", ()=>{
-   bodyColor.style.backgroundColor="wheat"
-   logoHeader.style.color ="red";
-
-   logoHeader.style.fontSize ="40px";
-   logoHeader.style.letterSpacing ="1.4rem";
-   bodyColor.style.textAlign ="center"
-})
-//nav bar background color , aligncenter and font
-
-
-let navbarParent = document.querySelector("nav");
-navbarParent.style.marginBottom = "20px"
-navbarParent.classList.add(".parent")
-navbarParent.children[0].classList.add("childrenDiv")
-navbarParent.children[1].classList.add("childrenDiv")
-navbarParent.children[2].classList.add("childrenDiv")
-navbarParent.children[3].classList.add("childrenDiv")
-
-navbarParent.children[0].addEventListener("mouseover",()=>{
-navbarParent.children[0].style.backgroundColor="red"
-})
-//header of the body cetnered
-let headerH2 = document.querySelectorAll("h2");
+ //Change the body color when it is loaded;
+  window.addEventListener("load", function(){
+ let body = document.querySelector("body");
+ body.style.padding=" 0,50px, 0 ,50px";
+  
+ //nav starts here
+ let mainNav = document.querySelector(".main-navigation");
+// console.log("main nav", mainNav)
+//  mainNav.style.width="100%";
+//  mainNav.style.height="90px";
+ mainNav.style.backgroundColor="white";
+ mainNav.style.borderBottom = "2px dashed black";
+ mainNav.style.margin ="0 auto";
+ mainNav.style.position="fixed";
+ let navBarParent = document.querySelector(".nav-container");
+  console.log("nav bar ", navBarParent);
+  navBarParent.style.display="flex";
+  navBarParent.style.flexDirection="row";
+  navBarParent.style.textAlign="center";
+  navBarParent.style.alignItems ="center";
+  navBarParent.style.height="100%";
+//   navBarParent.style.marginTop= 0;
+  navBarParent.style.justifyContent ="space-between";
+ let navBar = document.querySelectorAll("a");
+ navBar.forEach(x=>{
+     x.style.textDecoration="none";
+     x.style.padding ="20px";
+     x.style.color ="black";
+     x.style.fontSize="1.2rem"
+ })
+ //still working nav and h1
+ let headLog = document.querySelector("h1");
+ headLog.style.fontFamily="'Indie Flower', cursive";
+ headLog.style.padding="20px";
+//Done===========nav end here===================
+ //header starts here targeting h2
+ let headerH2 = document.querySelectorAll("h2");
 console.log(headerH2)
-headerH2.forEach(x=>{
-    x.style.color="red";
-    x.style.letterSpacing ="1.2rem";
-    x.style.textAlign="center"
-    
-})
+ headerH2.forEach(x=>{
+     x.style.color="red";
+     x.style.letterSpacing ="1.2rem";
+     x.style.textAlign="center"  
+ })
+//===================
 //logo image header image border-radius;
-let log = document.querySelector("header img");
-//  log.style.width="80%";
- log.style.marginTop ="5%"
-//  log.style.height="100%";
-//  log.style.borderRadius ="70px"
+let logContainer = document.querySelector(".home .intro img");
+logContainer.style.padding="15% 10% 0 10%";
+logContainer.style.display="block";
+ let log = document.querySelector("header img");
+     
+     log.style.height="100%";
+     log.style.borderRadius ="70px"
+let bodyOne = document.querySelector(".container");
+   
 
-
-//section images flex box advance flex-direction row
+// //section images flex box advance flex-direction row
 let images = document.querySelectorAll(".img-content");
 images.forEach(image=>{
-   
     image.addEventListener("mouseover", ()=>{
-        image.style.opacity = 0.5;
-        
+    image.style.opacity = 1; 
+    image.style.transform= "rotate(180deg)";
     })
 })
-//text content div first
+images.forEach(image=>{
+    image.addEventListener("mouseleave", ()=>{
+    image.style.opacity = 1;  
+    image.style.transform="rotate(360deg)"  
+    })
+})
+// //text content div first
 let textContentDiv = document.querySelectorAll(".text-content");
-textContentDiv.forEach(x=>{
-    x.addEventListener("select", ()=>{
-textContentDiv.forEach(item=>{
-item.style.width="50%";
-item.style.height="20%";
-item.style.backgroundColor="white"
-item.style.padding="40px";
-item.style.marginLeft="20%"
-item.style.border="4px solid black"
-item.style.opacity = 0.7;
-}) 
-    })
-})
 
-//div class content destination p 
-let contentDestination = document.querySelectorAll(".content-destination p");
-contentDestination[0].classList.add("welcomePar")
-//welcome paragraph 
-let welcomeP = document.querySelector("p");
-console.log(welcomeP)
-welcomeP.classList.add("welcomePar")
-
-let contentSection = document.querySelector(".content-section");
-contentSection.style.display="flex"
-console.log(contentSection,"name")
-contentSection.style.flex.direction ="column"
-
-
-
-
-//Fun in the sum paragraph and section content-pict formating
+//class content-pick as parent and three divs child flex them
+let contentDestination = document.querySelectorAll(".destination");
+  console.log("parent is ", contentDestination)
+  contentDestination[0].style.display="flex";
+  contentDestination[0].style.flexDirection="column";
+  contentDestination[0].style.textAlign="center"
+// //================================
+// //Fun in the sum paragraph and section content-pict formating
 let contentPick = document.querySelectorAll(".content-section");
 // contentPick[0].style.alignItems ="center"
 contentPick[0].children[0].style.backgroundColor ="gray";
@@ -98,7 +93,7 @@ footer.style.fontSize="48px";
 
 footer.style.width="100%";
 console.log(footer)
-// window.addEventListener("load", function(){
+
 let navContainer = document.querySelectorAll(".nav-link");
 navContainer.forEach(x=>{
     x.addEventListener("mouseover", ()=>{
@@ -131,16 +126,19 @@ document.body.addEventListener("mouseover", (event)=>{
 })
 function changeImage(event){
    console.log(event.target);
-   event.target.src ="img/barkon.jpeg"
+      event.preventDefault;
+//    event.target.src ="https://source.unsplash.com/random"
+      event.target.src ="./img/barkon.jpeg"
 }
-let imagebus = document.querySelector("img");
-   imagebus.addEventListener("dblclick", changeImage)
-// })
+let imageBus = document.querySelector("img");
+   imageBus.addEventListener("dblclick", changeImage)
+ 
 //dblclick on on the car image
 let keyPush = document.querySelector('body')
-keyPush.addEventListener('keydown', function(event) {
-    event.target.style.color = 'pink'
-    setTimeout(function() {
-        event.target.style.color = "";
-    }, 500);
-}, false);
+console.log("keypush",keyPush)
+keyPush.addEventListener('dblclick', function(event) {
+    event.target.style.color = 'pink';
+    keyPush.style.backgroundColor ="black";
+});   
+   
+ })
